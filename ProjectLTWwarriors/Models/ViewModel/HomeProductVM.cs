d58176dab13074e5edd16a961f8ddaa85f7a1e75
Legion <Legion@DESKTOP-VPHEA6L>
2025-11-22 21:41:14 +0700

    Add project files.

---
+++ ProjectLTWwarriors/Models/ViewModel/HomeProductVM.cs
@@ -0,0 +1,20 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectLTWwarriors.Models.ViewModel
+{
+    public class HomeProductVM
+    {
+        public string SearchTerm { get; set; }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 8;
+
+        public List<Products> FeaturedProducts { get; set; }
+
+        public IPagedList<Products> NewProducts { get; set; }
+    }
+}
